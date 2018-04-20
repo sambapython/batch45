1bacc61f0514d85b6ca5463145856db20cfa9431
sambapython@gmail.com <sambapython@gmail.com>
2018-04-20 09:29:44 +0530

    2018-04-20 looping

---
+++ loop.py
@@ -0,0 +1,7 @@
+# while and for
+i=0
+while i<10:
+    print "hello"
+    
+print "other statements in program"
+print "program ended"
---
+++ loop1.py
@@ -0,0 +1,9 @@
+print "program started"
+i=0
+while i<3:
+    continue
+    i=i+1
+    print "i inside loop:",i
+print "i outside loop",i
+print "other statements in program"
+print "program ended"
